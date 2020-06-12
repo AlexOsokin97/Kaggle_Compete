f19fb4dd0f6ca9b38635579da9e1b1c56e756121
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-06-12 06:40:07 +0300

    models, fixes

--- House Price/data_clean_spyder.py
+++ House Price/data_clean_spyder.py
@@ -8,40 +8,59 @@ Created on Tue Jun  9 00:40:55 2020
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('train.csv')
+df = pd.read_csv('new_train.csv')
 
-df2 = pd.read_csv('test.csv')
+df_dum = pd.get_dummies(df)
 
-TotalHouseSF = df['TotalBsmtSF'] + df['1stFlrSF'] + df['2ndFlrSF'] + df['GarageArea'] + df['OpenPorchSF'] + df['EnclosedPorch'] + df['WoodDeckSF'] + df['3SsnPorch'] + df['ScreenPorch'] + df['PoolArea']
+df_dum.drop(labels=['Unnamed: 0','AgeBeforeRemodel', 'MSZoning_RM', 'Street_Pave', 'LandContour_Lvl', 'Utilities_NoSeWa', 'LandSlope_Sev', 'Neighborhood_Veenker', 'Condition1_RRNn', 'Condition2_RRNn', 'BldgType_TwnhsE', 'HouseStyle_SLvl', 'Foundation_Wood', 'BsmtCond_TA', 'Heating_Wall', 'HeatingQC_TA', 'CentralAir_Y', 'Electrical_SBrkr', 'KitchenQual_TA', 'Functional_Typ', 'GarageType_NG', 'GarageQual_TA', 'SaleType_WD', 'SaleCondition_Partial'], axis=1, inplace=True)
 
+X = df_dum.drop(['SalePrice'], axis=1).values
+y = df_dum['SalePrice'].values
 
+from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-updated_df = df[['MSZoning','Street' ,'LandContour','Utilities','LandSlope','Neighborhood','Condition1','Condition2','BldgType','HouseStyle',
-                'OverallQual','OverallCond','YearBuilt','YearRemodAdd','Foundation','BsmtCond','TotalBsmtSF','Heating','HeatingQC','CentralAir',
-                'Electrical', '1stFlrSF', '2ndFlrSF', 'KitchenAbvGr','KitchenQual', 'TotRmsAbvGrd', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath',
-                'HalfBath','Functional','Fireplaces','GarageType', 'GarageYrBlt','GarageArea','GarageQual','MiscVal','MoSold',
-                'YrSold','SaleType','SaleCondition','SalePrice']]
+from sklearn.preprocessing import Normalizer
 
+n = Normalizer()
 
-TotalHouseSF = np.array(TotalHouseSF)
+X_train[:, 0:9] = n.fit_transform(X_train[:,0:9])
+X_test[:, 0:9] = n.transform(X_test[:, 0:9])
 
+##########################################################3
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression().fit(X_train, y_train)
 
-TotalBathRooms = df['BsmtFullBath'] + df['BsmtHalfBath'] + df['FullBath'] + df['HalfBath']
-TotalBathRooms = np.array(TotalBathRooms)
+reg.score(X_test, y_test)
 
-updated_df.info()
-updated_df['SaleType'].dtype
+y_pred = reg.predict(X_test)
 
-updated_df[['GarageType', 'GarageQual']] = updated_df[['GarageType', 'GarageQual']].apply(lambda x: x.fillna('NoGarage'))
+from sklearn.metrics import mean_squared_log_error
+rmsle = np.sqrt(mean_squared_log_error( y_test, y_pred))
+##############################################################
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
+rf = RandomForestRegressor(random_state=42)
 
+scores = np.mean(cross_val_score(estimator=rf, X=X_train, y=y_train,
+                                 scoring='neg_mean_squared_log_error', cv=3))
+print("avg_root_mean_squared_log_error: ", np.sqrt(scores*(-1)))
 
+#feature tuning
+params = [{'criterion':('mse','mae'), 'max_features':('sqrt','log2')}]
+gs = GridSearchCV(rf, params, scoring='neg_mean_squared_log_error', cv=3)
 
+gs.fit(X_train, y_train)
 
+np.sqrt(gs.best_score_*(-1))
+gs.best_params_
+gs.best_estimator_
 
-
-
-
+y_pred_rf = gs.best_estimator_.predict(X_test)
+rmsle_rf = np.sqrt(mean_squared_log_error( y_test, y_pred_rf))
+####################################################################
 
